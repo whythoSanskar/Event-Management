8838710b5da131f2d3c4ee5c1314206092c12c68
Sanskar <mishrasanskar@gmail.com>
2023-06-13 14:46:44 +0530

    home page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import { Routes, Route } from 'react-router-dom'
 import './App.css';
 import { About } from './Components/About';
 import { Home } from './Components/Home';
+import  Navbar  from './Components/Navbar';
 
 function App() {
   return (
+    <>
+    <Navbar/>
    <Routes>
     <Route path='/' element={<Home/>}/>
     <Route path={'about'} element={<About/>}/>
    </Routes>
+   </>
   );
 }
 
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,13 @@
 export const Home = () =>{
     return (
         <div>
-            Home Page
+            <img
+            className="w-full h-80 object-cover rounded-lg"
+                src="https://images.unsplash.com/photo-1473691955023-da1c49c95c78?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
+                alt="background image"
+            />
+
+            
         </div>
     )
 }
